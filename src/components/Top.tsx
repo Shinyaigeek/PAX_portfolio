7a99c119183253a631f3b1a77c53df30b7bc6c8c
Shinyaigeek <PhilisPaxil@gmail.com>
2020-03-15 12:03:25 +0900

    [initial commit]

---
+++ src/components/Top.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const Top = () => {
+    return (
+        <div className="top">
+
+        </div>
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { Top } from "../components/Top"
+
+export const Index = () => {
+    return (
+        <div className="index">
+            <Top />
+        </div>
+    )
+}
