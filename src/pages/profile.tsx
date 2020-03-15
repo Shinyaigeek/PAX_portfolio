e34cb87c01ad8c14ca088d9c837f945b53f1af92
Shinyaigeek <PhilisPaxil@gmail.com>
2020-03-15 19:57:08 +0900

    [fix]format

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -3,13 +3,7 @@ import React, { useState, useEffect } from "react";
 export const Profile = () => {
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
+  useEffect(() => {}, []);
 
-  },[]);
-
-  return (
-      <div className="page--profile">
-
-      </div>
-  )
+  return <div className="page--profile"></div>;
 };
