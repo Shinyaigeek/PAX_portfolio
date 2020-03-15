e858a40d9c26aafd98eb39dba24f1bbbe1b02bf8
Shinyaigeek <PhilisPaxil@gmail.com>
2020-03-15 20:01:49 +0900

    [fix]package

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -5,7 +5,7 @@ export const Profile = () => {
     <div className="profile">
       <h2>About me</h2>
       <div className="img">
-        <img src="./static/icon.jpg" className="icon" />
+        <img src="/static/icon.jpg" className="icon" />
       </div>
       <div className="profile">
         <div className="name">Chie Shiraishi</div>
