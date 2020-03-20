cf5c978adeb3ee42281e06bdfd633d6bbb8aa2c8
Shinyaigeek <PhilisPaxil@gmail.com>
2020-03-21 04:40:57 +0900

    [fix]

--- api/getWorks.ts
+++ api/getWorks.ts
@@ -1,16 +1,6 @@
 import fetch from "node-fetch";
 import { NowRequest, NowResponse } from "@now/node";
 
-const g = (a: any[], t: string) => {
-  for (let d of a) {
-    if (d.sys.id === t) {
-      return d.fields.file.url;
-    }
-  }
-
-  throw new Error("");
-};
-
 export default (_: NowRequest, s: NowResponse) => {
   const { CONTENTFUL_SPACE_ID, CONTENTFUL_ACCESS_TOKEN } = process.env;
   if (!CONTENTFUL_ACCESS_TOKEN || !CONTENTFUL_SPACE_ID) {
@@ -24,14 +14,6 @@ export default (_: NowRequest, s: NowResponse) => {
       return i
         .json()
         .then(e => {
-          // const u = e.items.map((q: any) => {
-          //   return {
-          //     name: q.fields.name,
-          //     description: q.fields.description,
-          //     src: g(e.includes.Asset, q.fields.work.sys.id)
-          //   };
-          // });
-
           s.json(e);
         })
         .catch(_ => {
