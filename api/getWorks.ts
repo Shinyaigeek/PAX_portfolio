230dfbafbb69530eee8e43ed8dd8f90be8abf306
Shinyaigeek <PhilisPaxil@gmail.com>
2020-03-21 04:24:50 +0900

    [fix]shape getWorks.ts

--- api/getWorks.ts
+++ api/getWorks.ts
@@ -1,63 +1,44 @@
 import fetch from "node-fetch";
 import { NowRequest, NowResponse } from "@now/node";
 
-export interface Work {
-  sys: {
-    id: string;
-    createdAt: string;
-    updatedAt: string;
-  };
-  fields: {
-    name: string;
-    description: string;
-    work: {
-      sys: {
-        id: string;
-      };
-    };
-  };
-}
-
-const getAsset = (assets: any[], target: string) => {
-  for (let asset of assets) {
-    if (asset.sys.id === target) {
-      return asset.fields.file.url;
+const g = (a: any[], t: string) => {
+  for (let d of a) {
+    if (d.sys.id === t) {
+      return d.fields.file.url;
     }
   }
 
-  throw new Error("there is no file in assets");
+  throw new Error("");
 };
 
-export default (req: NowRequest, res: NowResponse) => {
+export default (_: NowRequest, s: NowResponse) => {
   const { CONTENTFUL_SPACE_ID, CONTENTFUL_ACCESS_TOKEN } = process.env;
   if (!CONTENTFUL_ACCESS_TOKEN || !CONTENTFUL_SPACE_ID) {
-    throw new Error("Please check env variable");
+    throw new Error("");
   }
 
   return fetch(
     `https://cdn.contentful.com/spaces/${CONTENTFUL_SPACE_ID}/entries?access_token=${CONTENTFUL_ACCESS_TOKEN}&content_type=works`
   )
-    .then(item => {
-      return item
+    .then(i => {
+      return i
         .json()
-        .then(entries => {
-          const urls = entries.items.map((item: Work) => {
+        .then(e => {
+          const u = e.items.map((q: any) => {
             return {
-              name: item.fields.name,
-              description: item.fields.description,
-              src: getAsset(entries.includes.Asset, item.fields.work.sys.id)
+              name: q.fields.name,
+              description: q.fields.description,
+              src: g(e.includes.Asset, q.fields.work.sys.id)
             };
           });
 
-          res.json(urls);
+          s.json(u);
         })
-        .catch(err => {
-          console.log(err);
-          return false as false;
+        .catch(_ => {
+          return false;
         });
     })
-    .catch(err => {
-      console.log(err);
-      return false as false;
+    .catch(_ => {
+      return false;
     });
 };
