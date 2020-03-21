2752c8d1230dcb85f3ff29bd7035400b247ae7eb
Shinyaigeek <PhilisPaxil@gmail.com>
2020-03-22 00:25:21 +0900

    [add]name

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -6,11 +6,17 @@ import Skeleton from "@material-ui/lab/Skeleton";
 export const Profile = Layout(() => {
   const [isLoading, setIsLoading] = useState(true);
   const [bio, setBio] = useState("");
+  const [educationalBackground, setEducationalBackground] = useState([
+    "",
+    "",
+    ""
+  ] as string[]);
 
   useEffect(() => {
     fetch("/api/getProfile.ts").then(res => {
       res.json().then(json => {
         setBio(json.fields.bio);
+        setEducationalBackground(json.fields.educationalBackground);
         setIsLoading(false);
       });
     });
@@ -18,13 +24,37 @@ export const Profile = Layout(() => {
 
   return (
     <div className="page--profile">
+      <img src="/static/icon.jpg" className="icon" />
+      <div className="name">
+        <div className="en">Chie Shiraishi</div>
+        <div className="ja">白石　知愛</div>
+      </div>
+      <div className="workAbility">Illustrator</div>
       <div className="bio">
         {isLoading ? (
-          <Skeleton animation="wave" variant="rect" width={210} height={118} />
+          <Skeleton animation="wave" variant="rect" width="100%" height={300} />
         ) : (
           bio
         )}
       </div>
+      <ul className="educationalBackground">
+        {educationalBackground.map(background => {
+          return (
+            <li>
+              {isLoading ? (
+                <Skeleton
+                  animation="wave"
+                  variant="text"
+                  width="100%"
+                  height="100%"
+                />
+              ) : (
+                background
+              )}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 });
