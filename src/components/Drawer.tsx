537ed171cad44c78eaf0237133f4b05317ee5d8a
Shinyaigeek <PhilisPaxil@gmail.com>
2020-03-22 10:06:56 +0900

    [add]contact

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -14,7 +14,30 @@ export const Drawer = (props: Props) => {
         transform: `rotate(${props.drawerRotate}deg)`
       }}
     >
-      <div className="drawer">アイウエオ</div>
+      <div className="drawer">
+        <div className="drawer--content">
+          <ul className="drawer--list">
+            <li>
+              <a href="/" className="drawer--list__anchor">
+                HOME
+              </a>
+            </li>
+            <li>
+              <a href="/profile.html" className="drawer--list__anchor">
+                Profile
+              </a>
+            </li>
+            <li>
+              <a href="/gallery.html" className="drawer--list__anchor">
+                Gallery
+              </a>
+            </li>
+            <li>
+              <a href="mailto:">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
