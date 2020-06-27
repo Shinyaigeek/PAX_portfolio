a85d2d89e61f48e48aadb9d8f3fcee12e1f7b297
svpcadmin <PhilisPaxil@gmail.com>
2020-06-27 13:33:45 +0900

    add: history

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,6 +1,9 @@
 import * as React from "react";
 import { router } from "../router";
 import { render } from "react-dom";
+import createBrowserHistory from "history/createBrowserHistory";
+
+const history = createBrowserHistory();
 
 interface Props {
   to: string;
@@ -15,7 +18,7 @@ export const Link = (props: Props) => {
       className={`link ${props.class}`}
       id={props.id}
       href={`${props.to}`}
-      onClick={() => {
+      onClick={(evt: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
         router
           .resolve({
             pathname: props.to
@@ -26,9 +29,15 @@ export const Link = (props: Props) => {
               throw new Error("");
             }
             if (!document) {
-              throw new Error(")");
+              throw new Error("");
             }
-            console.log("aiueo");
+            evt.preventDefault();
+            if (
+              props.to !==
+              history.createHref(history.location).replace(location.origin, "")
+            ) {
+              history.push(props.to);
+            }
             render(component, document.getElementById("_app"));
           });
       }}
